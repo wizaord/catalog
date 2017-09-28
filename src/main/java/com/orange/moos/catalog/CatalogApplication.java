69940991ae01670aa44646fadeffda11616cb689
MOUILLERON Cedric <mouilleron.cedric@gmail.com>
2017-09-28 21:16:47 +0200

    Added banner + logback configuration + application yml

--- src/main/java/com/orange/moos/catalog/CatalogApplication.java
+++ src/main/java/com/orange/moos/catalog/CatalogApplication.java
@@ -1,12 +1,32 @@
 package com.orange.moos.catalog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
-@SpringBootApplication
+@SpringBootApplication //@EnableAutoConfiguration , @ComponentScan, SpringBootConfiguration
 public class CatalogApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(CatalogApplication.class);
+
+    private final Environment env;
+
+    public CatalogApplication(Environment env) {
+        this.env = env;
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(CatalogApplication.class, args);
+        SpringApplication app = new SpringApplication(CatalogApplication.class);
+        Environment env = app.run(args).getEnvironment();
+
+
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running!\n\t" +
+                        "Version '{}' \n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getActiveProfiles());
     }
 }
