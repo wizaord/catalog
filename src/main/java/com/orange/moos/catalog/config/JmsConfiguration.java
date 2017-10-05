001bcfdfa37142fada2466cae0728145ed4748b0
MOUILLERON Cedric <mouilleron.cedric@gmail.com>
2017-10-05 23:02:32 +0200

    Added TibcoEms connection Factory

--- src/main/java/com/orange/moos/catalog/config/JmsConfiguration.java
+++ src/main/java/com/orange/moos/catalog/config/JmsConfiguration.java
@@ -2,6 +2,7 @@ package com.orange.moos.catalog.config;
 
 import com.orange.moos.catalog.admin.E_PROFILES;
 import com.orange.moos.catalog.admin.Profiles;
+import com.tibco.tibjms.TibjmsConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
@@ -21,11 +23,20 @@ import javax.annotation.PostConstruct;
 import javax.jms.ConnectionFactory;
 
 import static com.orange.moos.catalog.admin.E_PROFILES.Constants.JMS;
+import static com.orange.moos.catalog.admin.E_PROFILES.Constants.TIBCOEMS;
 
 @Configuration
 @EnableJms
 @Profile(JMS)
 public class JmsConfiguration {
+    
+    public static final String ACTIVEMQ_BROKER = "spring.activemq.broker-url";
+    public static final String ACTIVEMQ_USER = "spring.activemq.user";
+    public static final String ACTIVEMQ_PASS = "spring.activemq.password";
+
+    public static final String TIBCOEMS_BROKER = "application.tibcoems.broker-url";
+    public static final String TIBCOEMS_USER = "application.tibcoems.user";
+    public static final String TIBCOEMS_PASS = "application.tibcoems.password";
 
     public static final String FACTORY_NAME = "jmsFactory";
 
@@ -34,20 +45,33 @@ public class JmsConfiguration {
     @Autowired
     private Profiles profiles;
 
+    @Autowired
+    private Environment env;
+
     @PostConstruct
     @DependsOn("profiles")
     public void log() {
         log.info("PostConstruct : Configuration JMS: successfully loaded");
         if (profiles.activesProfiles.contains(E_PROFILES.ACTIVEMQ)) {
             log.info("  Factory used : ACTIVEMQ");
+            log.info("  Broker Url : {}", env.getProperty(ACTIVEMQ_BROKER));
+            log.info("  login : {}", env.getProperty(ACTIVEMQ_USER));
         }
         if (profiles.activesProfiles.contains(E_PROFILES.TIBCOEMS)) {
             log.info("  Factory used : TIBCOEMS");
+            log.info("  Broker Url : {}", env.getProperty(TIBCOEMS_BROKER));
+            log.info("  login : {}", env.getProperty(TIBCOEMS_USER));
         }
-//        log.info("  - Addresses nodes: {}", String.join(",", this.appProperties.getAdresses()));
-//        log.info("  - rabbitMQ username: {}", this.appProperties.getUsername());
-//        log.info("  - rabbitMQ listener init consumers: {}", this.appProperties.getConcurrentConsumers());
-//        log.info("  - rabbitMQ listener max consumers: {}", this.appProperties.getMaxConcurrentConsumers());
+    }
+
+    @Bean
+    @Profile(TIBCOEMS)
+    public ConnectionFactory connectionFactory(){
+        log.info("  JMS configuration - Create TibcoEMS ConnectionFactory");
+        TibjmsConnectionFactory connectionFactory = new TibjmsConnectionFactory(env.getProperty(TIBCOEMS_BROKER));
+        connectionFactory.setUserName(env.getProperty(TIBCOEMS_USER));
+        connectionFactory.setUserPassword(env.getProperty(TIBCOEMS_PASS));
+        return connectionFactory;
     }
 
     @Bean
